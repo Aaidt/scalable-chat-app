1fcaa5c8e9312169623f49b8c481e85f21f0f284
Aadit <aadits2004@gmail.com>
2026-01-19 14:55:03 +0530

    awaiting connection of both the sockets using promises

--- user-ws/index.test.ts
+++ user-ws/index.test.ts
@@ -7,18 +7,39 @@ describe("Chat application", () => {
       const ws1 = new WebSocket(BACKEND_URL)
       const ws2 = new WebSocket(BACKEND_URL)
 
-      await new Promise<void>((resolve, reject) => {
-         let ctr = 0;
-         ws1.onopen = () => {
-            ctr += 1;
-            if (ctr == 2) resolve();
-         }
+      // await new Promise<void>((resolve, reject) => {
+      //    let ctr = 0;
+      //    ws1.onopen = () => {
+      //       ctr += 1;
+      //       if (ctr == 2) resolve();
+      //    }
+      //
+      //    ws2.onopen = () => {
+      //       ctr += 1;
+      //       if (ctr == 2) resolve();
+      //    }
+      // })
 
-         ws2.onopen = () => {
-            ctr += 1;
-            if (ctr == 2) resolve();
-         }
-      })
+      // await Promise.all([
+      //    new Promise(r => ws1.onopen = r),
+      //    new Promise(r => ws2.onopen = r)
+      // ]);
+
+      const timeout = 10 * 1000;
+
+      try {
+         await Promise.race([
+            Promise.all([
+               new Promise(res => ws1.onopen = res),
+               new Promise(res => ws2.onopen = res)
+            ]),
+            new Promise((_, rej) => {
+               setTimeout(rej, timeout)
+            })
+         ])
+      } catch (e) {
+         console.log(e);
+      }
 
       ws1.send(JSON.stringify({
          data: "hi there"
